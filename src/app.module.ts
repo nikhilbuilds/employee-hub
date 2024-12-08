6eed4f3264c1d0fc04a32d3f2e2a64f8d4f0a207
nikhilbuilds <devnikhil0306@gmail.com>
2024-12-09 00:34:41 +0530

    env added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,20 @@ import { EmployeeModule } from './modules/employee.module';
 import { UserModule } from './modules/user.module';
 import { CompanyModule } from './modules/company.module';
 import { TimeOffModule } from './modules/time-off.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigModule available throughout the application
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'employee_db',
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/entities/*{.entity.ts,.entity.js}'],
       //migrations: [__dirname + '/migrations/*{.ts,.js}'],
       synchronize: true,
